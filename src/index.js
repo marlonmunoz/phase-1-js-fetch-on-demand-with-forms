9944599820b91207e55eb5cc3f206838196a703a
Marlon Munoz <marlon@marlonshade.com>
2024-07-26 00:10:02 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form');
+    
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID')
+        console.log(input.value);
 
-document.addEventListener('DOMContentLoaded', init);
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((res) => res.json())
+        .then((movies) => {
+            const title = document.querySelector("section#movieDetails h4")
+            const summary = document.querySelector("section#movieDetails p")
+
+            title.textContent = movies.title;
+            summary.textContent = movies.summary;
+        });        
+    });
+};
+
